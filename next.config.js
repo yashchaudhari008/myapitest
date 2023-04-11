1476cce18893e8369071402eae5ebaf6ddced53a
Yash Chaudhari <yashchaudhari008@gmail.com>
2023-04-11 18:10:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = {
   async rewrites() {
     return [
       {
@@ -12,5 +10,4 @@ module.exports = {
     ]
   }
 }
-
 module.exports = nextConfig
